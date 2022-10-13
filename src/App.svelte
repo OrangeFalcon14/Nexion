6c9e994264b86a9862f989ece82e09f32dbae740
OrangeFalcon14 <vishrs020@gmail.com>
2022-10-13 16:47:43 +0530

    Added dock and dock icons

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+import Dock from './lib/Dock.svelte';
+import Window from './lib/Window.svelte';
+let apps_list = ["Files", "Firefox", "Alacritty", "Pycharm Community Edition", "Visual Studio Code"];
+</script>
+
+<div id="container">
+    <Dock {apps_list}/>
+</div>
---
+++ src/lib/Dock.svelte
@@ -0,0 +1,38 @@
+<script>
+import DockIcon from './components/DockIcon.svelte';
+
+export let apps_list; 
+</script>
+
+<div id="dock" class="panel-dock">
+    {#each apps_list as app}
+        <DockIcon app={app} on:newWindow/>
+    {/each}
+</div>
+
+<style>
+#dock {
+    user-select: none;
+    z-index: 10;
+    position: fixed;
+    background-color: var(--color1);
+}
+
+.panel-dock{
+    height: var(--dock-height);
+    width: 100%;
+    bottom: 0;
+}
+
+.compact-dock{
+    height: auto;
+    width: fit-content;
+    max-width: fit-content;
+    bottom: 1%;
+    left: 50%;
+    transform: translate(-50%, 0);
+    border-radius: 10px;
+    padding: 5px 10px 5px 10px;
+}
+
+</style>
