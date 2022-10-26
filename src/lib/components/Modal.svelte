cdacc9be4df33f13acd3b818f9119f373f843092
OrangeFalcon14 <vishrs020@gmail.com>
2022-10-26 12:42:49 +0530

    Improved text editor

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,34 @@
+<script>
+export let show_modal = false;
+</script>
+
+{#if show_modal}
+    <div class="backdrop">
+        <div class="modal">
+            <slot></slot>
+            <button on:click={() => show_modal = false}>Close</button>
+        </div>
+    </div>
+{/if}
+<style>
+.backdrop{
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0.5;
+    background: #000;
+}
+.modal{
+    position: relative;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 60%;
+    height: auto;
+    max-height: 60%;
+    opacity: 1;
+}
+</style>
--- src/lib/components/TextEditorWindow.svelte
+++ src/lib/components/TextEditorWindow.svelte
@@ -1,13 +1,18 @@
 <script>
 import ToolbarButton from "./ToolbarButton.svelte";
+import Modal from "./Modal.svelte";
 
 let reader = new FileReader();
 
 let text_content;
+let original_file_content;
 
 let text_wrap = "off";
 
 let file_name = "Untitled.txt";
+$: saved = original_file_content == text_content;
+
+let show_modal = false;
 
 const save_file = () => {
     let content = text_content;
@@ -16,19 +21,20 @@ const save_file = () => {
     a.href = window.URL.createObjectURL(blob);
     a.download = file_name;
     a.click();
-
+    saved = true;
 };
 
 const new_file = () => {
-    text_content = "";
+    if(saved) text_content = "";
+    else show_modal = true;
 };
 const open_file = (event) => {
     let file = event.detail.files[0];
-    console.log(file);
     reader.onload = () => {
-        text_content = reader.result;
+        text_content = original_file_content = reader.result.toString(); 
     }
     reader.readAsText(file)
+    file_name = file.name
 };
 </script>
 
@@ -39,6 +45,10 @@ const open_file = (event) => {
         <ToolbarButton on:fileOpen={open_file} text="" title="Open" />
     </div>
     <textarea wrap={text_wrap} bind:value={text_content} />
+    <Modal bind:show_modal>
+        <h1>You have unsaved work! </h1>
+        <h3>Your changes to {file_name} will be lost if you don't save them.</h3>
+    </Modal>
 </div>
 
 <style>
