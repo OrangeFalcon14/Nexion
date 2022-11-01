7b9ae17c8fbc2571053bef25b3f3aee142ea68df
OrangeFalcon14 <vishrs020@gmail.com>
2022-11-01 21:56:22 +0530

    Added Asteroids.html and sounds/ to dist

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 cp -R src/assets/fonts dist/src/assets/fonts
 cp -R src/assets/icons dist/src/assets/icons
 cp -R src/assets/wallpapers dist/src/assets/wallpapers
+cp -R src/assets/sounds dist/src/assets/sounds
+cp -R src/assets/Asteroids.html dist/src/assets/Asteroids.html
 
 git add dist -f 
 
