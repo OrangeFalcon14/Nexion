a23cfd7e48bb7f31adb99240c321b24ab0bdb4e3
OrangeFalcon14 <vishrs020@gmail.com>
2022-12-10 19:24:40 +0530

    Adding dist

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,15 @@ cp -R src/assets/wallpapers dist/src/assets/wallpapers
 cp -R src/assets/sounds dist/src/assets/sounds
 cp -R src/assets/Asteroids.html dist/src/assets/Asteroids.html
 
-git add dist -f 
+/usr/bin/rm -r ../dist
+cp -r dist ../dist
+git checkout gh-pages
+/usr/bin/rm -r src index.html
+cp -r ../dist/src src
+cp ../dist/index.html index.html
 
+git add .
 git commit -m "Adding dist"
+git push origin gh-pages
 
-git subtree push --prefix dist origin gh-pages
+git checkout svelte
