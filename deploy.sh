6de2c9347ea6efc73564ed5f1212b22d1d528d0f
OrangeFalcon14 <vishrs020@gmail.com>
2023-02-04 11:16:21 +0530

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 npm run build
 
-# mkdir -p dist/Nexion/
-
-# mv dist/fonts dist/Nexion/fonts
-# mv dist/icons dist/Nexion/icons
-# mv dist/wallpapers dist/Nexion/wallpapers
-# mv dist/sounds dist/Nexion/sounds
-# mv dist/Asteroids.html dist/Nexion/
-
 cd dist
 git init
 git checkout -B master
